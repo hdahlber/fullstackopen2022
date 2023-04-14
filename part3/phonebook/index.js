601f4cc6f0c5adbf0dceebf752f83f13566774d1
hdahlber <hdahlber@abo.fi>
2023-04-14 13:30:55 +0300

    3.13-3.14 done

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,56 +1,37 @@
+require("dotenv").config();
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
-app.use(express.static('build'))
+const mongoose = require("mongoose");
+const Person = require("./models/person")
 app.use(cors())
 app.use(express.json())
 app.use(express.static("build"))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :info'))
+
 morgan.token("info", (request, response) =>{
 
     return request.method ==="POST" ? JSON.stringify(request.body) :  request.method
 })
 
 
-
-
-let persons =[
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
-
 app.get('/info', (request, response) => {
     const date = new Date()
-    const personsLenght = persons.length
+    Person.find({}).then(person => {
     response.send(`<div>
-                            <p>Phonebook has info for ${personsLenght}</p>
+                            <p>Phonebook has info for ${person.length}</p>
                             <p>${date}</p>
                         </div>`)
+    })
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(person => {
+        response.json(person)
+    })
 })
-
+/*
 app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     const person = persons.find(person => person.id === id)
@@ -65,18 +46,33 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(person => person.id !== id)
 
     response.status(204).end()
-})
+})*/
 
 app.post('/api/persons', (request, response) => {
-    const randID = Number(Math.floor(Math.random() * Number.MAX_SAFE_INTEGER))
     const body = request.body
+    if(body.hasOwnProperty("name") && body.hasOwnProperty("number")){
+        const person = new Person({
+            name: body.name,
+            number: body.number
+        })
+        person.save().then(savedPerson => {
+            response.json(savedPerson)
+        })
+    }
+    else{
+        response.status(400)
+        response.send("Missing values need both name and number")
+    }
 
+
+    /*
+    const body = request.body
     if(body.hasOwnProperty("name") && body.hasOwnProperty("number")){
         const person = {
-            id: randID,
             name: body.name,
             number: body.number
         }
+
         if(persons.find((homonym)=>homonym.name === person.name)){
             response.status(400)
             response.send("name must be unique")
@@ -89,14 +85,14 @@ app.post('/api/persons', (request, response) => {
         response.status(400)
         response.send("Missing values need both name and number")
 
-    }
+    }*/
 })
 
 
 
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
