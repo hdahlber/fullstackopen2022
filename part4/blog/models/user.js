1f68ad8faca6aab81ef4072dbde2b691a45360fa
hdahlber <hdahlber@abo.fi>
2023-04-26 14:52:30 +0300

    4.18 and 4.19 done

--- part4/blog/models/user.js
+++ part4/blog/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-const uniqueValidator = require('mongoose-unique-validator')
+const uniqueValidator = require("mongoose-unique-validator")
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
@@ -12,8 +12,11 @@ const userSchema = new mongoose.Schema({
         required: true,
 
     },
-    passwordHash: String
-
+    passwordHash: String,
+    blogs: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Blog"
+    }]
 })
 
 userSchema.set("toJSON", {
